6995d112813151893dd16dd002f2b217e968a25c
LAPTOP-V82MHM7U\phill <phillip.kenzel@gmail.com>
2019-01-22 11:47:52 +0100

    Personenregister

---
+++ 06.pas
@@ -0,0 +1,90 @@
+program Test (input, output);
+{ Registrierung und Abfrage von Personen in einem Array mit Informationen zum jeweiligen Namen und Alter. }
+
+	type
+	tIndex = integer;
+	tPerson = record
+				Name : string;
+				Alter : integer
+			end;
+	tEintrag = array [tIndex] of tPerson;
+
+	var
+	NeuerEintrag : tEintrag;
+	Ergebnis : tPerson;
+	Ordnungsnummer,
+	RufAlter,
+	Suchwert : tIndex;
+	RufName,
+	Antwort : string;
+
+begin
+	writeln('Hallo! Mit diesem Programm kannst du Personen in ein Register eintragen. Bitte vergebe fuer die erste Person eine Ordnungsnummer und beginne bei 1. Du kannst jederzeit durch eine "0" den Vorgang abbrechen.');
+	readln(Ordnungsnummer);
+	if Ordnungsnummer = 1 then
+	begin
+		writeln('Wie heisst die Person?');
+		readln(RufName);
+		NeuerEintrag[Ordnungsnummer].Name := RufName;
+		writeln('Wie alt ist die Person?');
+		readln(RufAlter);
+		NeuerEintrag[Ordnungsnummer].Alter := RufAlter;
+		writeln('Bitte vergebe die naechste Ordnungsnummer. Erinnerung: Mit der Eingabe "0" kannst du den Vorgang abbrechen.');
+		readln(Ordnungsnummer);
+
+		while Ordnungsnummer <> 0 do
+		begin
+			writeln('Wie heisst die Person?');
+			readln(RufName);
+			NeuerEintrag[Ordnungsnummer].Name := RufName;
+			writeln('Wie alt ist die Person?');
+			readln(RufAlter);
+			NeuerEintrag[Ordnungsnummer].Alter := RufAlter;
+			writeln('Bitte vergebe die naechste Ordnungsnummer. Erinnerung: Mit der Eingabe "0" kannst du den Vorgang abbrechen.');
+			readln(Ordnungsnummer);
+		end;
+	end
+	else
+		writeln('Danke fuer deine Zeit. Der Vorgang wird abgebrochen ...');
+
+	writeln('ENDE der Registrierung.');
+	writeln('...');
+	writeln('...');
+	writeln('...');
+
+	writeln('Moechtest du nun eine Abfrage aus diesem Register starten? Antworte mit "JA" oder "NEIN".');
+	readln(Antwort);
+	if Antwort = 'JA' then
+	begin
+		writeln('...');
+		writeln('...');
+		writeln('...');
+		writeln('START der Abfrage.');
+		writeln('...');
+		writeln('...');
+		writeln('...');
+		writeln('Welche Person suchst du? Bitte gib ihre Ordnungsnummer an. Du kannst diesen Prozess wieder mit Eingabe einer "0" abbrechen.');
+		readln(Suchwert);
+		Ergebnis := NeuerEintrag[Suchwert];
+		writeln('Die Person heisst: ', Ergebnis.Name, '.');
+		writeln('Die Person ist ', Ergebnis.Alter, ' Jahre alt.');
+
+		writeln('Welche weitere Person suchst du? Bitte gib ihre Ordnungsnummer an.');
+		writeln('Denk dran: Wenn du nicht fortfahren willst, einfach eine "0" eingeben.');
+		readln(Suchwert);
+
+		while Suchwert <> 0 do
+		begin
+			Ergebnis := NeuerEintrag[Suchwert];
+			writeln('Die Person heisst: ', Ergebnis.Name, '.');
+			writeln('Die Person ist ', Ergebnis.Alter, ' Jahre alt.');
+			writeln('Welche weitere Person suchst du? Bitte gib ihre Ordnungsnummer an.');
+			writeln('Denk dran: Wenn du nicht fortfahren willst, einfach eine "0" eingeben.');
+			readln(Suchwert);
+		end;
+
+		writeln('Die Abfrage endet nun. Auf Wiedersehen!');
+	end
+	else
+		writeln('Schade, dann bis zum naechsten Mal!')
+end.
